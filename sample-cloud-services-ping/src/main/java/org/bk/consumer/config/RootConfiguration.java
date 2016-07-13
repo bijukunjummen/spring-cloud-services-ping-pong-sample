6fe602c286875bb74d95a70bd3d35a8d93b838c7
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2016-07-12 21:16:03 -0700

    With SCS 1.1.0

--- sample-cloud-services-ping/src/main/java/org/bk/consumer/config/RootConfiguration.java
+++ sample-cloud-services-ping/src/main/java/org/bk/consumer/config/RootConfiguration.java
@@ -8,10 +8,10 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RootConfiguration {
 
-//    @Bean
-//    @LoadBalanced
-//    public RestTemplate restTemplate() {
-//        return new RestTemplate();
-//    }
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
